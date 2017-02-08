ebddb1ddb25ae4db2cc03dacd0201da2df5edb21
a.kluev <a.kluev@php73.ru>
2017-02-08 14:23:37 +0400

    First component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LessonOneComponent } from "./lesson-one/lesson-one.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LessonOneComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/lesson-one/lesson-one.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'mediasoft-lesson-one',
+    templateUrl: 'lesson-one.component.html'
+})
+export class LessonOneComponent implements OnInit {
+    constructor() {
+    }
+
+    ngOnInit() {
+
+    }
+}
