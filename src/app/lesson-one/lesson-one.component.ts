d81cdc8ec7656773a7c335d76b94288656b492c2
a.kluev <a.kluev@php73.ru>
2017-02-08 16:53:06 +0400

    ngFor

--- src/app/lesson-one/lesson-one.component.ts
+++ src/app/lesson-one/lesson-one.component.ts
@@ -5,10 +5,26 @@ import {Component, OnInit} from '@angular/core';
     templateUrl: 'lesson-one.component.html'
 })
 export class LessonOneComponent implements OnInit {
+
+    public personName = '';
+    public personBackground = 'white';
+    public persons = [
+        {name: 'Alice', background: 'white'},
+        {name: 'Bob', background: 'white'}
+    ];
+
     constructor() {
     }
 
     ngOnInit() {
+    }
 
+    public addPerson() {
+        this.persons.push({name: this.personName, background: this.personBackground});
+        this.personName = '';
+    }
+
+    public removePerson(person) {
+        this.persons.splice(this.persons.indexOf(person), 1)
     }
 }
