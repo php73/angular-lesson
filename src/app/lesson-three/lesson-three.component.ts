879dd1cbc6693e72b4ecacd6f975697a5a43cd1d
a.kluev <a.kluev@php73.ru>
2017-02-20 16:42:22 +0400

    Reactive forms

--- src/app/lesson-three/lesson-three.component.ts
+++ src/app/lesson-three/lesson-three.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup, FormControl } from "@angular/forms";
+import { equals } from "./equals-validator/equals.validator";
 
 @Component({
     selector: 'mediasoft-lesson-three',
@@ -6,12 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LessonThreeComponent implements OnInit {
 
-    public formEnabled: boolean = true;
     public passwordVisible: boolean = false;
     public passwordRepeatVisible: boolean = false;
     public user: any = {};
 
-    constructor() { }
+    public registrationForm: FormGroup;
+    public username: FormControl;
+    public password: FormControl;
+    public passwordRepeat: FormControl;
+
+    constructor(formBuilder: FormBuilder) {
+
+        this.username = new FormControl('', [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(10),
+            Validators.pattern(/^[a-zA-Z]+$/),
+        ]);
+
+        this.password = new FormControl('', Validators.required);
+        this.passwordRepeat = new FormControl('', equals(this.password));
+
+        this.registrationForm = formBuilder.group({
+            username: this.username,
+            passwords: formBuilder.group({
+                password: this.password,
+                passwordRepeat: this.passwordRepeat
+            })
+        });
+
+    }
 
     ngOnInit() { }
 
--- src/app/lesson-three/lesson-three.module.ts
+++ src/app/lesson-three/lesson-three.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { LessonThreeComponent } from './lesson-three.component';
 import { RouterModule } from "@angular/router";
 import { HttpModule } from "@angular/http";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { BrowserModule } from "@angular/platform-browser";
 import { EqualsValidatorDirective } from "./equals-validator/equals.directive";
@@ -12,7 +12,7 @@ import { EqualsValidatorDirective } from "./equals-validator/equals.directive";
     imports: [
         BrowserModule,
         CommonModule,
-        FormsModule,
+        ReactiveFormsModule,
         HttpModule,
         RouterModule.forChild([
             {
